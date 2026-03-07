db3edcd631e625ef6195203d5ddf1000f64ac403
Clarice <105030558+claririe@users.noreply.github.com>
2026-03-07 11:43:20 -0500

    Initial skeleton

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div style={{ padding: "24px", textAlign: "center" }}>
+      <p>bonk</p>
+    </div>
+  )
+}
+
+
+export default App
